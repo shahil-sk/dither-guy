5da7c481efeb87bd9bb046fd4e547e512b0ed30e
Shahil Ahmed (sk) <93572629+shahil-sk@users.noreply.github.com>
2026-05-05 22:50:12 +0530

    Update VERSION to 3.5.4

--- utils/constants.py
+++ utils/constants.py
@@ -7,7 +7,7 @@ _HISTORY_LIMIT = 20
 _DEBOUNCE_MS   = 250
 _PREVIEW_SCALE = 0.5
 _VIDEO_WORKERS = max(4, (os.cpu_count() - 1 or 4))
-VERSION        = "3.5.2"
+VERSION        = "3.5.4"
 
 METHOD_GROUPS = {
     "Ordered": [
